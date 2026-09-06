eacb131fc4403575a51f0203b0b0c6411269d5c9
Naveen Rajapaksha <navee.rajapaksha83@gmail.com>
2026-09-06 13:00:23 +0530

    Initial Libora backend setup

---
+++ Backend/src/main/java/com/libora/backend/LiboraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.libora.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiboraBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiboraBackendApplication.class, args);
+    }
+
+}
